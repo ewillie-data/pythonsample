b0080a73881ebebe128d125bb55ea3ad6e71726a
Emmanuel Willie <eewillie1@gmail.com>
2020-06-17 13:58:32 +0200

    first commit

---
+++ area_rect.py
@@ -0,0 +1,13 @@
+#This code calculates the area of a rectangle
+
+#width = 10 #this is the width of the rectangle
+#length = 20 # this is the length of the rectangle
+#area_rectangle = length * width
+#print(area_rectangle)
+
+width = int(input("Enter your width"))
+length = int(input("Enter your length"))
+
+area = (length * width)
+print("The area is:", area)
+ 
---
+++ area_triangle.py
@@ -0,0 +1,20 @@
+# calculate the area of a triangle
+
+
+#the code below from line 5 - 11 is to show how putting the values works
+#base = 6 
+#height = 8
+#area = base * height
+
+#area_triang = area / 2
+
+#print(area_triang)
+
+#the code below show how to gt input values from a user
+
+base = int(input("Enter your base"))
+height = int(input("Enter your height"))
+
+area = (base * height) / 2
+print(area)
+
